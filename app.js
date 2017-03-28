9b188caf78176163280c20c9b8cc67b0ac174f99
j-west <westja89@gmail.com>
2017-03-28 11:57:36 -0500

    Add products array in app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@
 const express = require('express');
 const app = express();
 
+const bakedGoods = [
+                    {
+                     name: "Mystery Muffin",
+                     price: "$1.50"
+                    },
+                    {
+                    name: "Banana Beer Baked Bagel",
+                    price: "$4.75"
+                    },
+                    {
+                    name: "Baked creol Bread",
+                    price: "$2.25"
+                    },
+                    {
+                    name: "Baked Apple",
+                    price: "$.99"
+                    },
+                    {
+                    name: "Tambo's Special Toast",
+                    price: "$3.50"
+                    }
+                   ];
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', './views');
@@ -9,16 +32,17 @@ app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
- res.render('index', {title: "Home"});
-})
+ res.render('index', {title: 'Home'});
+});
 
 app.get('/about', (req, res) => {
- res.render('about', {title: "About"});
-})
+  res.render('about', {title: 'About'});
+});
 
 app.get('/inventory', (req, res) => {
-  res.render('inventory', {title: "Inventory"});
-})
+  res.render('inventory', {title: 'Inventory', products: bakedGoods });
+
+});
 
 
 app.listen(8080);
